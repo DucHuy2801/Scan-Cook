644ade6e65bef546da571939475ef0c9b7420cd4
DucHuy2801 <theflash28012002@gmail.com>
2023-12-04 09:41:21 +0700

    add recogniteRecipe

--- src/modules/disk/disk.controller.ts
+++ src/modules/disk/disk.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Query } from '@nestjs/common';
 import { DiskService } from './disk.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateDiskDTO } from './dto/create-disk.dto';
+import { query } from 'express';
+import { MatchDTO } from './dto/recipe-match.dto';
 
 @ApiTags('disk')
 @Controller('disk')
@@ -18,8 +20,27 @@ export class DiskController {
         return this.diskService.getAllDisks()
     }
 
+    @Post('/recipe-matching')
+    matchRecipe(@Body() body: MatchDTO){
+        return {
+            data: "Matching recipe successfully!"
+        }
+    }
+
     @Get(':id')
     getDiskById(@Param('id') id: number) {
         return this.diskService.getDiskById(id)
+    }
+
+    @Get('/search')
+    searchDisk(@Query('query') query: string){
+        return {
+            "disks": [
+                {
+                  "id": "string",
+                  "name": "string"
+                }
+            ]
+        }
     }
 }
