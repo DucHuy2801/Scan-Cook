fb8a1ce6d2f95fe1d0f72faf9c81b08e9a7be7f6
DucHuy2801 <theflash28012002@gmail.com>
2023-12-04 09:41:43 +0700

    add recognition

--- src/modules/recipe/recipe.controller.ts
+++ src/modules/recipe/recipe.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { RecipeService } from './recipe.service';
 
 @ApiTags('recipe')
 @Controller('recipe')
 export class RecipeController {
     constructor(private readonly recipeService: RecipeService) {}
+
+    @Post('/recognition')
+    @ApiProperty()
+    recogniteRecipe(@Body() image: string) {
+        return {
+            "recipes": [
+                {
+                  "id": "string",
+                  "name": "string",
+                  "confident": 0
+                }
+            ]
+        }
+    }
 
     @Get(':id')
     getRecipeById(@Param('id') id: number) {
